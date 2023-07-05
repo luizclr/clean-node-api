93b62be766ef04b65ee1becbe845a3e16473ac85
Luiz Claudio <luiz.lclr@gmail.com>
2023-07-05 00:23:35 -0300

    test(log-controller): refactor to remove duplicated code

--- tests/main/decorators/log-controller/log-controller.spec.ts
+++ tests/main/decorators/log-controller/log-controller.spec.ts
@@ -2,9 +2,15 @@ import { makeSutTypes } from "#/main/decorators/log-controller/types";
 import { StatusCodes } from "http-status-codes";
 import { LogErrorRepository } from "~/data/protocols/log-error-repository";
 import { LogControllerDecorator } from "~/main/decorators/log-decorator/log-controller";
-import { serverError } from "~/presentation/helpers/http-helper";
+import { ok, serverError } from "~/presentation/helpers/http-helper";
 import { Controller } from "~/presentation/protocols/controller";
 import { HttpRequest, HttpResponse } from "~/presentation/protocols/http";
+
+const makeHttpRequest = (): HttpRequest => ({
+  body: {
+    name: "name",
+  },
+});
 
 class ControllerStub implements Controller {
   handle(_httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -46,23 +52,14 @@ describe("LogControllerDecorator", () => {
     // given
     const { sut, controllerStub } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, "handle");
-    const httpRequest: HttpRequest = {
-      body: {
-        name: "name",
-      },
-    };
+    const httpRequest = makeHttpRequest();
 
     // when
     const httpResponse = await sut.handle(httpRequest);
 
     // then
     expect(handleSpy).toBeCalledWith(httpRequest);
-    expect(httpResponse).toEqual({
-      statusCode: StatusCodes.OK,
-      body: {
-        name: "name",
-      },
-    });
+    expect(httpResponse).toEqual(ok(httpRequest.body));
   });
 
   it("should call LogErrorRepository with correct error if controller returns a server error", async () => {
@@ -76,11 +73,7 @@ describe("LogControllerDecorator", () => {
       .spyOn(controllerStub, "handle")
       .mockReturnValueOnce(Promise.resolve(error));
 
-    const httpRequest: HttpRequest = {
-      body: {
-        name: "name",
-      },
-    };
+    const httpRequest = makeHttpRequest();
 
     // when
     await sut.handle(httpRequest);
