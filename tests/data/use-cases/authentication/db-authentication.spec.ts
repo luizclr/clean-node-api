b07269bfefa7237ed5a6ffa64357e9c64ddb97f3
Luiz Claudio <luiz.lclr@gmail.com>
2023-07-07 16:20:21 -0300

    test(DbAuthentication): sndure AbAuthentication returns null if GetAccountByEmailRepository returns null

--- tests/data/use-cases/authentication/db-authentication.spec.ts
+++ tests/data/use-cases/authentication/db-authentication.spec.ts
@@ -83,134 +83,170 @@ const makeSut = (): makeSutTypes => {
 
 // eslint-disable-next-line max-lines-per-function
 describe("DbAuthentication use case", () => {
-  it("should call GetAccountByEmailRepository with correct e-mail", async () => {
-    // given
-    const { sut, getAccountByEmailRepositoryStub } = makeSut();
-    jest
-      .spyOn(getAccountByEmailRepositoryStub, "get")
-      .mockReturnValueOnce(Promise.reject(new Error()));
+  describe("GetAccountByEmailRepository", () => {
+    it("should call GetAccountByEmailRepository with correct e-mail", async () => {
+      // given
+      const { sut, getAccountByEmailRepositoryStub } = makeSut();
+      jest
+        .spyOn(getAccountByEmailRepositoryStub, "get")
+        .mockReturnValueOnce(Promise.reject(new Error()));
 
-    // when
-    const promise = sut.auth(email, password);
+      // when
+      const promise = sut.auth(email, password);
 
-    // then
-    await expect(promise).rejects.toThrow();
+      // then
+      await expect(promise).rejects.toThrow();
+    });
+
+    it("should throw if GetAccountByEmailRepository throws", async () => {
+      // given
+      const { sut, getAccountByEmailRepositoryStub } = makeSut();
+      jest
+        .spyOn(getAccountByEmailRepositoryStub, "get")
+        .mockReturnValueOnce(Promise.reject(new Error()));
+
+      // when
+      const promise = sut.auth(email, password);
+
+      // then
+      await expect(promise).rejects.toThrow();
+    });
+
+    it("should return null if GetAccountByEmailRepository returns null", async () => {
+      // given
+      const { sut, getAccountByEmailRepositoryStub } = makeSut();
+      jest
+        .spyOn(getAccountByEmailRepositoryStub, "get")
+        .mockReturnValueOnce(Promise.resolve(null));
+
+      // when
+      const accessToken = await sut.auth(email, password);
+
+      // then
+      await expect(accessToken).toBeNull();
+    });
   });
 
-  it("should call HashComparer with correct values", async () => {
-    // given
-    const { sut, hashComparerStub } = makeSut();
-    const compareSpy = jest.spyOn(hashComparerStub, "compare");
+  describe("HashComparer", () => {
+    it("should call HashComparer with correct values", async () => {
+      // given
+      const { sut, hashComparerStub } = makeSut();
+      const compareSpy = jest.spyOn(hashComparerStub, "compare");
 
-    // when
-    await sut.auth(email, password);
+      // when
+      await sut.auth(email, password);
 
-    // then
-    expect(compareSpy).toHaveBeenCalledWith(password, hashedPassword);
+      // then
+      expect(compareSpy).toHaveBeenCalledWith(password, hashedPassword);
+    });
+
+    it("should throw if HashComparer throws", async () => {
+      // given
+      const { sut, hashComparerStub } = makeSut();
+      jest
+        .spyOn(hashComparerStub, "compare")
+        .mockReturnValueOnce(Promise.reject(new Error()));
+
+      // when
+      const promise = sut.auth(email, password);
+
+      // then
+      await expect(promise).rejects.toThrow();
+    });
+
+    it("should return null if HashComparer returns false", async () => {
+      // given
+      const { sut, hashComparerStub } = makeSut();
+      jest
+        .spyOn(hashComparerStub, "compare")
+        .mockReturnValueOnce(Promise.resolve(false));
+
+      // when
+      const accessToken = await sut.auth(email, password);
+
+      // then
+      await expect(accessToken).toBeNull();
+    });
   });
 
-  it("should throw if HashComparer throws", async () => {
-    // given
-    const { sut, hashComparerStub } = makeSut();
-    jest
-      .spyOn(hashComparerStub, "compare")
-      .mockReturnValueOnce(Promise.reject(new Error()));
+  describe("TokenGenerator", () => {
+    it("should call TokenGenerator with correct id", async () => {
+      // given
+      const { sut, tokenGeneratorStub } = makeSut();
+      const generateSpy = jest.spyOn(tokenGeneratorStub, "generate");
 
-    // when
-    const promise = sut.auth(email, password);
+      // when
+      await sut.auth(email, password);
 
-    // then
-    await expect(promise).rejects.toThrow();
+      // then
+      expect(generateSpy).toHaveBeenCalledWith(id);
+    });
+
+    it("should throw if TokenGenerator throws", async () => {
+      // given
+      const { sut, tokenGeneratorStub } = makeSut();
+      jest
+        .spyOn(tokenGeneratorStub, "generate")
+        .mockReturnValueOnce(Promise.reject(new Error()));
+
+      // when
+      const promise = sut.auth(email, password);
+
+      // then
+      await expect(promise).rejects.toThrow();
+    });
+
+    it("should return null if TokenGenerator returns false", async () => {
+      // given
+      const { sut, tokenGeneratorStub } = makeSut();
+      jest
+        .spyOn(tokenGeneratorStub, "generate")
+        .mockReturnValueOnce(Promise.resolve(null));
+
+      // when
+      const accessToken = await sut.auth(email, password);
+
+      // then
+      await expect(accessToken).toBeNull();
+    });
+
+    it("should generate correct accessToken", async () => {
+      // given
+      const { sut } = makeSut();
+
+      // when
+      const accessToken = await sut.auth(email, password);
+
+      // then
+      await expect(accessToken).toBe(token);
+    });
   });
 
-  it("should return null if HashComparer returns false", async () => {
-    // given
-    const { sut, hashComparerStub } = makeSut();
-    jest
-      .spyOn(hashComparerStub, "compare")
-      .mockReturnValueOnce(Promise.resolve(false));
+  describe("updateAccessTokenRepository", () => {
+    it("should call updateAccessTokenRepository with correct values", async () => {
+      // given
+      const { sut, updateAccessTokenRepositoryStub } = makeSut();
+      const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, "update");
 
-    // when
-    const accessToken = await sut.auth(email, password);
+      // when
+      await sut.auth(email, password);
 
-    // then
-    await expect(accessToken).toBeNull();
-  });
+      // then
+      await expect(updateSpy).toHaveBeenCalledWith(id, token);
+    });
 
-  it("should call TokenGenerator with correct id", async () => {
-    // given
-    const { sut, tokenGeneratorStub } = makeSut();
-    const generateSpy = jest.spyOn(tokenGeneratorStub, "generate");
+    it("should throw if updateAccessTokenRepository throws", async () => {
+      // given
+      const { sut, updateAccessTokenRepositoryStub } = makeSut();
+      jest
+        .spyOn(updateAccessTokenRepositoryStub, "update")
+        .mockReturnValueOnce(Promise.reject(new Error()));
 
-    // when
-    await sut.auth(email, password);
+      // when
+      const promise = sut.auth(email, password);
 
-    // then
-    expect(generateSpy).toHaveBeenCalledWith(id);
-  });
-
-  it("should throw if TokenGenerator throws", async () => {
-    // given
-    const { sut, tokenGeneratorStub } = makeSut();
-    jest
-      .spyOn(tokenGeneratorStub, "generate")
-      .mockReturnValueOnce(Promise.reject(new Error()));
-
-    // when
-    const promise = sut.auth(email, password);
-
-    // then
-    await expect(promise).rejects.toThrow();
-  });
-
-  it("should return null if TokenGenerator returns false", async () => {
-    // given
-    const { sut, tokenGeneratorStub } = makeSut();
-    jest
-      .spyOn(tokenGeneratorStub, "generate")
-      .mockReturnValueOnce(Promise.resolve(null));
-
-    // when
-    const accessToken = await sut.auth(email, password);
-
-    // then
-    await expect(accessToken).toBeNull();
-  });
-
-  it("should generate correct accessToken", async () => {
-    // given
-    const { sut } = makeSut();
-
-    // when
-    const accessToken = await sut.auth(email, password);
-
-    // then
-    await expect(accessToken).toBe(token);
-  });
-
-  it("should call updateAccessTokenRepository with correct values", async () => {
-    // given
-    const { sut, updateAccessTokenRepositoryStub } = makeSut();
-    const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, "update");
-
-    // when
-    await sut.auth(email, password);
-
-    // then
-    await expect(updateSpy).toHaveBeenCalledWith(id, token);
-  });
-
-  it("should throw if updateAccessTokenRepository throws", async () => {
-    // given
-    const { sut, updateAccessTokenRepositoryStub } = makeSut();
-    jest
-      .spyOn(updateAccessTokenRepositoryStub, "update")
-      .mockReturnValueOnce(Promise.reject(new Error()));
-
-    // when
-    const promise = sut.auth(email, password);
-
-    // then
-    await expect(promise).rejects.toThrow();
+      // then
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
