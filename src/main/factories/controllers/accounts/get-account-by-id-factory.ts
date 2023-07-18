c6c514db65c9bdd73aa0ea065227b7c53f3d49d1
Luiz Claudio <luiz.lclr@gmail.com>
2023-07-18 10:14:32 -0300

    feat(UUIDValidator): add instance into controller factory

--- src/main/factories/controllers/accounts/get-account-by-id-factory.ts
+++ src/main/factories/controllers/accounts/get-account-by-id-factory.ts
@@ -1,3 +1,4 @@
+import { UUIDValidatorAdapter } from "~/infra/validators/uuid-validator-adapter";
 import { LogControllerDecorator } from "~/main/decorators/log-decorator/log-controller";
 import { makeDbLogErrorRepository } from "~/main/factories/protocols/log-error-repository/db-log-error-repository-factory";
 import { makeDbGetAccountById } from "~/main/factories/use-cases/get-account-by-id/db-get-accounts-factory";
@@ -5,9 +6,11 @@ import { GetAccountByIdController } from "~/presentation/controllers/accounts/ge
 import { Controller } from "~/presentation/protocols/controller";
 
 export const makeGetAccountByIdController = (): Controller => {
+  const uuidValidator = new UUIDValidatorAdapter();
   const dbGetAccountById = makeDbGetAccountById();
   const getAccountByIdController = new GetAccountByIdController(
-    dbGetAccountById
+    dbGetAccountById,
+    uuidValidator
   );
   const logPgRepository = makeDbLogErrorRepository();
 
